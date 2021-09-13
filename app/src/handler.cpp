46c350fe4e9420801d83e564bc0f8b424315e391
Tushar Harsora <tusharharsora95@gmail.com>
2021-09-13 12:55:33 +0530

    wrote demo for parsing json using nlohmann::json

--- app/src/handler.cpp
+++ app/src/handler.cpp
@@ -1,4 +1,5 @@
 #include "../include/handler.h"
+#include "../include/json.hpp"
 
 handler::handler(utility::string_t url) : listener(url) {
     listener.support(methods::GET, std::bind(&handler::handle_get, this, std::placeholders::_1));
@@ -7,12 +8,13 @@ handler::handler(utility::string_t url) : listener(url) {
 }
 
 void handler::handle_get(http_request message) {
-    ucout << message.to_string() << endl;
-    message.reply(status_codes::OK, message.to_string());
+    ucout << "Fragment of uri : " << message.absolute_uri().path() << endl;
+    string body = message.extract_utf8string(true).get();
+    nlohmann::json req = nlohmann::json::parse(body);
+    message.reply(status_codes::OK, body);
 };
 
 void handler::handle_post(http_request message) {
-    ucout << message.to_string() << endl;
     message.reply(status_codes::OK, message.to_string());
 };
 
